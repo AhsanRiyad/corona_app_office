e507f1471410bcbc5187bd5e57107f4b6b669ae7
unknown <riyad298@gmail.com>
2020-04-20 13:50:13 +0600

    commit by alias
    this is used for short cut puspose

---
+++ src/views/pages/Login.vue
@@ -0,0 +1,97 @@
+<template>
+  <CContainer class="d-flex content-center min-vh-100">
+    <CRow>
+      <CCol>
+        <CCardGroup>
+          <CCard class="p-4">
+            <CCardBody>
+              <CForm>
+                <h1>Login</h1>
+                <p class="text-muted">Sign In to your account</p>
+                <CInput
+                placeholder="Username"
+                autocomplete="username email"
+                >
+                <template #prepend-content><CIcon name="cil-user"/></template>
+              </CInput>
+              <CInput
+              placeholder="Password"
+              type="password"
+              autocomplete="curent-password"
+              >
+              <template #prepend-content><CIcon name="cil-lock-locked"/></template>
+            </CInput>
+            <CRow>
+              <CCol col="6" class="text-left">
+                <CButton color="primary" class="px-4">Login</CButton>
+              </CCol>
+              <CCol col="6" class="text-right">
+                <CButton color="link" class="px-0">Forgot password?</CButton>
+                <CButton color="link" class="d-md-none">Register now!</CButton>
+              </CCol>
+            </CRow>
+          </CForm>
+        </CCardBody>
+      </CCard>
+      <CCard
+      color="primary"
+      text-color="white"
+      class="text-center py-5 d-sm-down-none"
+      body-wrapper
+      >
+      <h2>Sign up</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <CButton
+      color="primary"
+      class="active mt-3"
+      >
+      Register
+    </CButton>
+  </CCard>
+</CCardGroup>
+</CCol>
+</CRow>
+</CContainer>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    components: {},
+    data: ()=>({
+      name: 'Riyad',
+    }),
+    computed:{},
+    methods: {
+      get_login_info(){
+
+        let headers = {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Accept': 'application/json'} ;
+
+          this.$axios.get( 'http://localhost/backend_all/laravel_computer_shop/public/get_invoice_info/11122' , {
+
+          } , headers )
+          .then(function(response){
+            console.log(response);
+          }.bind(this))
+          .catch(function () {
+
+          }.bind(this)); 
+
+        }
+      },
+      created(){
+
+        this.get_login_info();
+
+      },
+      mounted(){},
+      updated(){
+      // console.log(this.selected);
+    }
+
+
+
+  }
+</script>
